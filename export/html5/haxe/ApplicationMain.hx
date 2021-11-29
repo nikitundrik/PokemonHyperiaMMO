7e5f46755543a3245d5b164351fcbc5116e47a4c
Nikita Petrov <local_kings@ctemplar.com>
2021-11-29 17:26:46 +1000

    Added Server.hx file

--- export/html5/haxe/ApplicationMain.hx
+++ export/html5/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "116";
+		app.meta["build"] = "1";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "PokemonHyperiaMMO";
 		app.meta["name"] = "PokemonHyperiaMMO";
